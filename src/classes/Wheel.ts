ad8b2d9b85d98d816b56abfe494a2585a2729780
MChance176 <matt.chance176@gmail.com>
2024-11-15 11:44:14 -0500

    Add src directory contents

---
+++ src/classes/Wheel.ts
@@ -0,0 +1,34 @@
+// Wheel class that defines the properties of a wheel
+class Wheel {
+  // Declare properties of the Wheel class using private access modifier
+  private diameter: number;
+  private tireBrand: string;
+
+  // Constructor for the Wheel class
+  constructor(diameter: number = 18, tireBrand: string = "GoodYear") {
+    this.diameter = diameter;
+    this.tireBrand = tireBrand;
+  }
+
+  // Getter methods for the the diameter property  
+  get getDiameter(): number {
+    return this.diameter;
+  }
+   // Getter method for the tireBrand property  
+  get getTireBrand(): string {
+    return this.tireBrand;
+  }
+}
+  // Setter method for the diameter property
+  set setDiameter(diameter: number) {  
+    this.diameter = diameter;  
+   }  
+   
+   // Setter method for the tireBrand property  
+   set setTireBrand(tireBrand: string) {  
+    this.tireBrand = tireBrand;  
+   }  
+ }  
+
+// Export the Wheel class
+export default Wheel;
